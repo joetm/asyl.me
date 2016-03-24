b479b3a34f330acd5acb75cf5cbb3c143388bf5b
J. Oppenlaender <j.oppenlaender@web.de>
2016-03-24 08:58:59 +0000

    line draw test

--- js/main.js
+++ js/main.js
@@ -126,6 +126,7 @@ $(function(){
     });
 
 
+
     // -------------------------------------------------------------
     // AJAX queries
     // -------------------------------------------------------------
@@ -585,5 +586,20 @@ $(function(){
         alert('reloading');
         console.log('reloading');
     });
+
+
+//testing: drawing an arc
+var start = { x: 36.8027059, y: 34.794319 };
+var end = { x: -0.3817789, y: 51.528308 };
+var generator = new arc.GreatCircle(start, end, { name: 'Syria migration 1' });
+var line = generator.Arc(100, { offset: 10 });
+L.geoJson(line.json()).addTo(map);
+
+var start = { x: 36.8027059, y: 34.794319 };
+var end = { x: 5.9677381, y: 51.0782036 };
+var generator = new arc.GreatCircle(start, end, { name: 'Syria migration 1' });
+var line = generator.Arc(100, { offset: 10 });
+L.geoJson(line.json()).addTo(map);
+
 
 }());
